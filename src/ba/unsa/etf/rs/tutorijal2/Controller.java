e7919b2df003b43917eace77eb36cdb73a152b9f
Tarik Sijerčić <tsijercic1@etf.unsa.ba>
2020-03-17 16:17:57 +0100

    Povezan Controller sa sample.fxml

--- src/ba/unsa/etf/rs/tutorijal2/Controller.java
+++ src/ba/unsa/etf/rs/tutorijal2/Controller.java
@@ -1,4 +1,16 @@
 package ba.unsa.etf.rs.tutorijal2;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+
 public class Controller {
+
+    public Label prezimeLabela;
+
+    public void onClick(ActionEvent actionEvent) {
+        System.out.println("Button is pressed!");
+        prezimeLabela.setText(prezimeLabela.getText() + prezimeLabela.getText());
+
+    }
+
 }
